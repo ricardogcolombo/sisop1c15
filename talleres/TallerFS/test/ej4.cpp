5f4c0f8defb528d702e284b2ffcbbe8d1d4011a2
Franco Negri <franconegri2004@gmail.com>
2015-05-24 19:08:49 -0300

    Agregando todo lo que tengo en el repo

--- talleres/TallerFS/test/ej4.cpp
+++ talleres/TallerFS/test/ej4.cpp
@@ -18,13 +18,13 @@ int main(int argc, char ** argv)
 
 	Ext2FSInode *inodo_de_la_nota = fs->get_file_inode_from_dir_inode(inodoPath,"nota.txt");
 
-	unsigned int dir = fs->get_block_address(inodo_de_la_nota, 10);
+	unsigned int dir = fs->get_block_address(inodo_de_la_nota, 0);
 
-	unsigned char *bufer;
+	unsigned char bufer[10000];
 
 	fs->read_block(dir, bufer);
 
-	cout << (void*)&bufer;
+	printf("%s\n", bufer);
 
 	return 0;
 }
